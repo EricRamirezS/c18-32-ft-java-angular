2966e57a3e17390e0d10048cd54c6f0c328fd031
Juan Rivera <jmrest87@gmail.com>
2024-05-20 10:34:18 -0300

    holaMundo

---
+++ c18-32-ft-java-angular/src/main/java/tech/nocountry/c1832ftjavaangular/C1832FtJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package tech.nocountry.c1832ftjavaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1832FtJavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1832FtJavaAngularApplication.class, args);
+	}
+
+}
---
+++ c18-32-ft-java-angular/src/test/java/tech/nocountry/c1832ftjavaangular/C1832FtJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nocountry.c1832ftjavaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1832FtJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
