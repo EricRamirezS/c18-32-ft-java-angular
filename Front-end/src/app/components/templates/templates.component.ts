ab98ddcf14835d8c9e4c5d7f97b5d1ddb7580155
CarlosMrtnzMrtnz <carlosmartinezmartinez2015@gmail.com>
2024-05-20 12:56:30 -0500

    Creacion de rama Carlos y componentes

---
+++ Front-end/src/app/components/templates/templates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-templates',
+  standalone: true,
+  imports: [],
+  templateUrl: './templates.component.html',
+  styleUrl: './templates.component.css'
+})
+export class TemplatesComponent {
+
+}
