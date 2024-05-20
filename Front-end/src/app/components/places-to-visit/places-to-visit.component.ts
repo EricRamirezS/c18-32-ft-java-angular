1ff38e7da0db592ab7120a19d572bf2f1ace9a11
Paula <paulaoorjuela@gmail.com>
2024-05-20 11:50:21 -0500

    Configuración del frontend con Angular

---
+++ Front-end/src/app/components/places-to-visit/places-to-visit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-places-to-visit',
+  standalone: true,
+  imports: [],
+  templateUrl: './places-to-visit.component.html',
+  styleUrl: './places-to-visit.component.css'
+})
+export class PlacesToVisitComponent {
+
+}
---
+++ Front-end/src/app/components/voting-page/voting-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voting-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './voting-page.component.html',
+  styleUrl: './voting-page.component.css'
+})
+export class VotingPageComponent {
+
+}
