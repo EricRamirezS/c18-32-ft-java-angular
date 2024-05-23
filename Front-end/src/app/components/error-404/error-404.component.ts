d661f39db8750fdf390e3053ec90b22f61907e01
Paula <paulaoorjuela@gmail.com>
2024-05-23 10:44:35 -0500

    PDOVG-79

--- Front-end/src/app/components/error-404/error-404.component.ts
+++ Front-end/src/app/components/error-404/error-404.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error-404',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './error-404.component.html',
   styleUrl: './error-404.component.css'
 })
