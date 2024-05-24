88055665b001e9b184349ce79cc74c164a61ed48
Paula <paulaoorjuela@gmail.com>
2024-05-24 13:29:38 -0500

    PDOVG-49

--- Front-end/src/app/components/navbar/navbar.component.ts
+++ Front-end/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink, ReactiveFormsModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+    formForgotPass: FormGroup;
+    emailRegex = /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/
 
+    constructor(private fb: FormBuilder) {
+        this.formForgotPass = this.fb.group({
+            email: ['', [Validators.required, Validators.pattern(this.emailRegex)]]
+        })
+    }
+
+
+    submitFormForgotPass(){
+
+    }
 }
