d066adb6457943b67a4c2afe469d6e3faafe90c3
DMorozco <dianaorozco0830@gmail.com>
2024-05-24 16:55:55 +0200

    modales

--- Front-end/src/app/components/places-to-visit/places-to-visit.component.ts
+++ Front-end/src/app/components/places-to-visit/places-to-visit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-places-to-visit',
@@ -7,6 +7,49 @@ import { Component } from '@angular/core';
   templateUrl: './places-to-visit.component.html',
   styleUrl: './places-to-visit.component.css'
 })
-export class PlacesToVisitComponent {
+export class PlacesToVisitComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.setupModal();
+  }
+
+  setupModal(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      const modalButtons = document.querySelectorAll<HTMLButtonElement>('.ver-lista-btn');
+      const modals = document.querySelectorAll<HTMLElement>('.modal');
+      const closeButtons = document.querySelectorAll<HTMLButtonElement>('.close-btn');
+
+      modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          const modalId = button.dataset['modalTarget'];
+          if (modalId) {
+            const modal = document.querySelector<HTMLElement>(modalId);
+            if (modal) {
+              modal.style.display = 'block';
+            }
+          }
+        });
+      });
+
+      closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          const modal = button.closest('.modal');
+          if (modal) {
+            modal.classList.remove('show'); // Agrega esta línea
+          }
+        });
+      });
+      
+
+      window.addEventListener('click', (event) => {
+        modals.forEach(modal => {
+          if (event.target == modal) {
+            modal.style.display = 'none';
+          }
+        });
+      });
+    });
+  }
 }
+
+
